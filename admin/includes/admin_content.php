28ace4279168cbbecea5fe0b5b4d764b1038c05d
Sixeye <srinath@srinath.fr>
2019-11-27 11:37:14 +0100

    admin db

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -4,8 +4,15 @@
     <div class="row">
         <div class="col-lg-12">
             <h1 class="page-header">
-                Blank Page
+                ADMIN
                 <small>Subheading</small>
+                <?php
+                $sql = "SELECT * FROM users WHERE id =1";
+                $result = $database->query($sql);
+                $user_found = mysqli_fetch_array($result);
+                echo $user_found['firstname'], " ",  $user_found['lastname'];
+                ?>
+
             </h1>
             <ol class="breadcrumb">
                 <li>
--- admin/includes/top_nav.php
+++ admin/includes/top_nav.php
@@ -6,7 +6,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">Off Shooting Admin</a>
+                <a class="navbar-brand" href="../index.php">Off Shooting Admin</a>
             </div>
             <!-- Top Menu Items -->
             <ul class="nav navbar-right top-nav">
@@ -37,7 +37,7 @@
                                     </span>
                                     <div class="media-body">
                                         <h5 class="media-heading">
-                                            <strong>John Smith</strong>
+                                            <strong>Tyler Durden</strong>
                                         </h5>
                                         <p class="small text-muted"><i class="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>
                                         <p>Lorem ipsum dolor sit amet, consectetur...</p>
@@ -94,7 +94,7 @@
                     </ul>
                 </li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Tyler Durden <b class="caret"></b></a>
                     <ul class="dropdown-menu">
                         <li>
                             <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
